e11f8501dcced55df34854c81e1bc6e76e1539e4
birblett <124317329+birblett@users.noreply.github.com>
2025-01-04 15:31:57 -0800

    rename count function

--- day_19_clojure/run.clj
+++ day_19_clojure/run.clj
@@ -1,10 +1,10 @@
-(defn parse_in [] (to-array (clojure.string/split (slurp "in.txt") #"\n\n" )))
-(def patterns (clojure.string/split (aget (parse_in) 0) #", "))
-(def towels (clojure.string/split (aget (parse_in) 1) #"\n"))
+(def input (to-array (clojure.string/split (slurp "in.txt") #"\n\n" )))
+(def patterns (clojure.string/split (aget input 0) #", "))
+(def towels (clojure.string/split (aget input 1) #"\n"))
 
-(def memct (memoize (fn [str]
+(def ct (memoize (fn [str]
   (cond
     (= (count str) 0) 1
-    :else (reduce + (map (fn [s] (cond (clojure.string/starts-with? str s) (memct (subs str (count s))) :else 0)) patterns))))))
+    :else (reduce + (map (fn [s] (cond (clojure.string/starts-with? str s) (ct (subs str (count s))) :else 0)) patterns))))))
 
-(println (reduce (fn [i, j] (cond (> j 0) (+ i 1) :else i)) (map memct towels)) (reduce + (map memct towels)))
+(println (reduce (fn [i, j] (cond (> j 0) (+ i 1) :else i)) (map ct towels)) (reduce + (map ct towels)))
