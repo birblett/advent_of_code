846925dcfffa399d0f7f818fec894600b89e7f1c
birblett <124317329+birblett@users.noreply.github.com>
2025-01-30 16:58:13 -0800

    trim day 9

--- day_09_rust/run.rb
+++ day_09_rust/run.rb
@@ -1,19 +1,13 @@
-require "../timer"
-
-timed(10) {
-puts File.open("in.txt") { |f, vals = [], bad_blocks = [], good_blocks = [], res = 0, res2 = 0, left = 0, i = 0|
+puts File.open("in.txt") { |f, vals = [], bad_blocks = [], good_blocks = [], res = 0, res2 = 0, left = 0, i = 0, right|
   f.map(&:strip).map(&:chars).each.map { |l| l.map(&:to_i) }[0].each_with_index { |j, k|
      k & 1 == 0 ? (good_blocks.insert(0, [(vals.length...vals.length + j), k >> 1])) : (bad_blocks.unshift(vals.length...vals.length + j) unless j == 0)
      (0...j).each { vals.push(k & 1 == 0 ? k >> 1 : -1) }
-   }
-  right = vals.length - 1
-  while left < right
-    if vals[left] == -1
-      (0..).each { break true unless (vals[right] == -1) and (right -= 1)}
-      vals[left], vals[right] = vals[right], -1
-    end
-    res += vals[(left += 1) - 1] * ((i += 1) - 1)
+  }
+  (if vals[left] == -1
+    (0..).each { break right unless (vals[right] == -1) and (right -= 1)}
+    vals[left], vals[right] = vals[right], -1
   end
+  res += vals[(left += 1) - 1] * ((i += 1) - 1)) while left < (right ||= vals.length - 1)
   good_blocks.each { |range, idx, l = 0|
     (while (p = bad_blocks[l -= 1])
        ((sz2 = (rng = p.end - (p.size - (sz = range.size))...p.end).size)
@@ -22,6 +16,5 @@ puts File.open("in.txt") { |f, vals = [], bad_blocks = [], good_blocks = [], res
        (p.begin...p.begin + sz).each { |k| res2 += k * idx }
        break true) unless p.begin >= range.begin or p.size < range.size
      end) || (range.each { |k| res2 += k * idx })
-  } and [res, res2]
-}
+  } && [res, res2]
 }
