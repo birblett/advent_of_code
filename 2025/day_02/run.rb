acbd4606014573e6a72105d5ea2339b6a4c40499
birblett <bwee.sylv@gmail.com>
2025-12-12 02:47:54 -0800

    small edits d2

--- 2025/day_02/run.rb
+++ 2025/day_02/run.rb
@@ -1,16 +1,14 @@
 FACTORS_NONE = (1..10).map { |i| i.even? ? [i, [i / 2]] : [i, []] }.to_h
 FACTORS = (1..10).map { |i| [i, (1..i / 2).filter { |i2| i % i2 == 0 }] }.to_h
 
-def expand(base, mul, count)
-  (0...count).reduce(base) { |summed, _| summed * mul + base }
-end
+def expand(base, mul, count) = (0...count).reduce(base) { |summed, _| summed * mul + base }
 
 def invalid(range, factors)
-  [[range[0].length, (range[0].to_i..[10 ** range[0].length - 1, range[1].to_i].min)], [range[1].length, ([10 ** range[0].length, range[1].to_i].min..range[1].to_i)]].map { |digits, range, found = []|
-    factors[digits].each { |d, current = (range.min / 10 ** (digits - d) - 1)|
-      while (n = expand(current += 1, 10 ** d, digits / d - 1)) <= range.max
-        found.push(n) if range.include?(n)
-      end
+  lhs = [range[0].length, (range[0].to_i..[10 ** range[0].length - 1, range[1].to_i].min)]
+  rhs = [range[1].length, ([10 ** range[0].length, range[1].to_i].min..range[1].to_i)]
+  [lhs, rhs].map { |digits, rng, found = []|
+    factors[digits].each { |d, current = (rng.min / 10 ** (digits - d) - 1), n|
+      (found.push(n) if rng.include?(n)) while (n = expand(current += 1, 10 ** d, digits / d - 1)) <= rng.max
     }
     found
   }.reduce(&:+)
